f54839b64a84d431a055d6a1bc5fdd29033cbc53
Brian Henning <henninb@msn.com>
2023-03-15 05:11:33 -0500

    wip

--- run.sh
+++ run.sh
@@ -1,24 +1,40 @@
 #!/bin/sh
 
-# stack run
-if command -v pacman; then
+if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
   sudo pacman --noconfirm --needed -S gobject-introspection
-elif command -v apt; then
+  sudo pacman --noconfirm --needed -S gobject-introspection-runtime
+  sudo pacman --noconfirm --needed -S gdk-pixbuf2
+  sudo pacman --noconfirm --needed -S harfbuzz
+  sudo pacman --noconfirm --needed -S harfbuzz-cairo
+elif [ "$OS" = "Gentoo" ]; then
+  sudo emerge --update --newuse harfbuzz
+elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
   sudo apt install -y gobject-introspection
   sudo apt install -y libatk1.0-dev
   sudo apt install -y libpango1.0-dev
   sudo apt install -y libgdk-pixbuf-2.0-dev
   sudo apt install -y libgirepository1.0-dev
   sudo apt install -y libgtk-3-dev
-elif command -v dnf; then
+elif [ "$OS" = "Void" ]; then
+  sudo xbps-install -y gobject-introspection
+elif [ "$OS" = "FreeBSD" ]; then
+  echo "sudo pkg install -y"
+elif [ "$OS" = "Solus" ]; then
+  "sudo eopkg install -y"
+elif [ "$OS" = "openSUSE Tumbleweed" ]; then
+  sudo zypper install -y gobject-introspection-devel
+  sudo zypper install -y pango-devel
+elif [ "$OS" = "Fedora Linux" ]; then
   sudo dnf install -y gobject-introspection-devel
   sudo dnf install -y cairo-gobject-devel
   sudo dnf install -y atk-devel
   sudo dnf install -y gdk-pixbuf2-devel
   sudo dnf install -y pango-devel
   sudo dnf install -y gtk3-devel
-elif command -v xbps-install; then
-  sudo xbps-install -y gobject-introspection
+elif [ "$OS" = "Clear Linux OS" ]; then
+  "sudo swupd bundle-add"
+elif [ "$OS" = "Darwin" ]; then
+  echo "brew install"
 else
   echo "$OS is not yet implemented."
   exit 1
